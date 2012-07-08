c07ccc8c26d023c31cdfde87ad3d6d4b5c0174be
AdiPat <adithemagnificent@gmail.com>
2012-07-08 11:04:52 +0530

    Enable TINY_SHMEM

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
+echo "Initiating TITANIUM COMPILER"
+echo ">>>>>>>>>>>Start>>>>>>>>>>>>>"
 make clean mrproper
 make titanium_defconfig
+echo "Enter Kernel version"
+read vrsn
+echo $vrsn > .version
 make -j32
 cd ..
 ./copy.sh
 ./repack.sh
-
+cd out
+echo "Enter Kernel name"
+read name;
+if test -e boot.img ; then
+zip -r $name.zip META-INF system boot.img
+else
+echo "No boot.img found"
+echo "Terminating process"
+fi 
